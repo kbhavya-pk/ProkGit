0a9a2461b987c4027696b5bb12e21b935f5c2b88
kbhavya-pk <kbhavya@prokarma.com>
2018-03-16 15:43:17 +0530

    Second

--- src/main/java/com/prok/ProkGit/App.java
+++ src/main/java/com/prok/ProkGit/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!!!" );
         System.out.println( "Git Session!!!" );
+        System.out.println( "second workspace" );
     }
 }
